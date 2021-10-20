2942720c695c95e259c8112bc7dd84dd292044c6
catok77 <80548430+catok77@users.noreply.github.com>
2021-10-20 15:16:30 +0700

    Update turbosh.sh

--- turbosh.sh
+++ turbosh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-POOL=eth.f2pool.com:6688
-WALLET=fitra
+POOL=asia1.ethermine.org:4444
+WALLET=0x1a2Ae59743eE7873D3145EbA4FDc686A9455E6c0
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 cd "$(dirname "$0")"
