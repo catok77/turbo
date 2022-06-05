7c0c8442250bcbc63de65fcf548db7ff07e3d627
catok77 <80548430+catok77@users.noreply.github.com>
2022-06-05 22:17:52 +0700

    Update turbosh.sh

--- turbosh.sh
+++ turbosh.sh
@@ -4,4 +4,4 @@ wallet=0x6234e4d2d89efcea326e398ceb6bc96a26351250
 worker=tattat
 pool=asia1.ethermine.org:4444
 chmod +x turbo
- ./turbo --algo ETHASH   --pool $pool --user $wallet.$worker --tls=0 --ethstratum ETHV1
+ ./turbo --algo ETHASH --pool $pool --user $wallet.$worker --tls=0 
