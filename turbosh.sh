c55653bf808afcd5c9cb5840e9a305f2ca5c4b11
catok77 <80548430+catok77@users.noreply.github.com>
2022-06-07 19:51:45 +0700

    Update turbosh.sh

--- turbosh.sh
+++ turbosh.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-wallet=3PJeAvRAywKzaUFwteqvret6V3PsQJvTmi
-worker1=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
-ramu=184.181.217.206:4145
-pool=stratum+ssl://beamv3.auto.nicehash.com:443
-chmod +x turbo
-./turbo --algo BEAM-III --pool $pool --user $wallet.$worker1 
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=beam.sunpool.top:3334
+WALLET=32f2e8765c2e8f5ea41becc5f397024c94d80cc5fc50ee917af23b260ecb3a5f.testRun
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+./lolMiner --coin BEAM --pool $POOL --user $WALLET $@
