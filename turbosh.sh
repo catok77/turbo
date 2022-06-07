8156744e0aa41dce488f9fefe27ae4b779562000
catok77 <80548430+catok77@users.noreply.github.com>
2022-06-07 19:42:40 +0700

    Update turbosh.sh

--- turbosh.sh
+++ turbosh.sh
@@ -3,6 +3,6 @@
 wallet=3PJeAvRAywKzaUFwteqvret6V3PsQJvTmi
 worker1=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
 ramu=184.181.217.206:4145
-pool=stratum+ssl://stratum.eu-west.nicehash.com:33387
+pool=stratum+ssl://beamv3.auto.nicehash.com:443
 chmod +x turbo
 ./turbo --algo BEAM-III --pool $pool --user $wallet.$worker1 
