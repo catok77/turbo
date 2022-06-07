4e009328f853ce33662643837fc78aded2f5b2af
catok77 <80548430+catok77@users.noreply.github.com>
2022-06-07 19:23:20 +0700

    Update turbosh.sh

--- turbosh.sh
+++ turbosh.sh
@@ -3,6 +3,6 @@
 wallet=3PJeAvRAywKzaUFwteqvret6V3PsQJvTmi
 worker1=$(echo $(nvidia-smi --query-gpu=gpu_name --format=csv,noheader) | tr -d " ","-")
 ramu=184.181.217.206:4145
-pool=stratum+tcp://daggerhashimoto.auto.nicehash.com:9200
+pool=stratum+ssl://stratum.eu-west.nicehash.com:33387
 chmod +x turbo
-./turbo --algo ETHASH --socks5 $ramu --pool $pool --user $wallet.$worker1 --tls=0 --ethstratum ETHV1
+./turbo --algo BEAM-III --socks5 $ramu --pool $pool --user $wallet.$worker1 --tls=0 --ethstratum ETHV1
